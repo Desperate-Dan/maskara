37f843a488f8fee7c04bde89f328e53ed4d6f17a
Desperate-Dan <dmmalone@tcd.ie>
2022-11-03 13:20:44 +0000

    Make repo pip installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+import glob
+import os
+import pkg_resources
+
+from maskara import __version__, _program
+
+setup(
+    name='maskara',
+    version='0.0.1',    
+    description='maskara',
+    url='https://github.com/Desperate-Dan/maskara',
+    author='Daniel Maloney',
+    author_email='dmaloney@ed.ac.uk',
+    packages=find_packages(),
+    scripts=['maskara/maskara.py'],
+    install_requires=['biopython',
+                        "pysam",
+                        "numpy"
+                      ],
+    entry_points="""
+    [console_scripts]
+    {program} = maskara:main
+    """.format(program = _program)
+)
