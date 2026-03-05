234368ae81c498df451ce97151ba910787e840d0
Desperate-Dan <dmmalone@tcd.ie>
2026-03-05 15:04:28 +0000

    Adding ability to generate masks for multiple refs in the bam file simultaneously.

--- maskara/__init__.py
+++ maskara/__init__.py
@@ -1,2 +1,2 @@
 _program = "maskara"
-__version__ = "1.1.3"
+__version__ = "1.1.4"
--- maskara/maskara.py
+++ maskara/maskara.py
@@ -10,78 +10,96 @@ import pysam
 import sys
 
 #Main body of the function
+def ref_finder(aln_file,reads):
+    ref_stats = aln_file.get_index_statistics()
+    refs_above_threshold = {}
+    for ref in ref_stats:
+        if ref[1] > int(reads):
+            refs_above_threshold[ref[0]] = ref[1]
+    return refs_above_threshold
 
 def runner(args):
     #Open the alignment file - expects BAM at the moment
     aln_file = pysam.AlignmentFile(args.input_file,'rb')
+
+    if args.mmm:
+        refs_above_threshold = ref_finder(aln_file,args.reads)
+        if not refs_above_threshold:
+            print (f"No refs with more than {args.reads} reads!")
+            sys.exit(0)
+    else:
+        refs_above_threshold = [args.ref_name]
     
-    #Make a dictionary of length = to the ref sequence
-    coverage_dict = {}
-    seq_length = aln_file.get_reference_length(args.ref_name)
-    for i in range(0 ,seq_length):
-        coverage_dict[i] = 0
-    
-    #Populate that dictionary with the read depth at each position
-    for pileup_column in aln_file.pileup(args.ref_name, truncate=False, min_base_quality=0):
-        coverage_dict[pileup_column.pos] = pileup_column.n
-    
-    #Create a list of lists containing the runs of positions below the "depth" value
-    mask_pos_list = []
-    position_list = []
-    #The below could definitely be written more succinctly but it works...
-    for pos in coverage_dict:
-        if not args.inverse: # Added to give sites with at least X depth in bed format for samtools view
-            if pos + 1 != seq_length: # Added to make sure last block to be masked is added to the file
-                if coverage_dict[pos] < int(args.depth):
+    for ref in refs_above_threshold:
+        #Make a dictionary of length = to the ref sequence
+        coverage_dict = {}
+        seq_length = aln_file.get_reference_length(ref)
+        for i in range(0 ,seq_length):
+            coverage_dict[i] = 0
+        
+        #Populate that dictionary with the read depth at each position
+        for pileup_column in aln_file.pileup(ref, truncate=False, min_base_quality=0):
+            coverage_dict[pileup_column.pos] = pileup_column.n
+        
+        #Create a list of lists containing the runs of positions below the "depth" value
+        mask_pos_list = []
+        position_list = []
+        #The below could definitely be written more succinctly but it works...
+        for pos in coverage_dict:
+            if not args.inverse: # Added to give sites with at least X depth in bed format for samtools view
+                if pos + 1 != seq_length: # Added to make sure last block to be masked is added to the file
+                    if coverage_dict[pos] < int(args.depth):
+                        position_list.append(pos)
+                    else:
+                        if position_list:
+                            mask_pos_list.append(position_list)
+                        position_list = []
+                else:
+                    position_list.append(pos)
+                    mask_pos_list.append(position_list)
+            else:
+                if coverage_dict[pos] >= int(args.depth):
                     position_list.append(pos)
                 else:
                     if position_list:
                         mask_pos_list.append(position_list)
                     position_list = []
-            else:
-                position_list.append(pos)
-                mask_pos_list.append(position_list)
-        else:
-            if coverage_dict[pos] >= int(args.depth):
-                position_list.append(pos)
-            else:
-                if position_list:
-                    mask_pos_list.append(position_list)
-                position_list = []
-    
-    #If there are no poisitons above the threshold
-    if not mask_pos_list:
-        no_cov = True
-        if not args.inverse:
-            mask_pos_list.append([0,seq_length - 1]) # -1 to preserve function with the 0vs1 base bit below
+        
+        #If there are no poisitons above the threshold
+        if not mask_pos_list:
+            no_cov = True
+            if not args.inverse:
+                mask_pos_list.append([0,seq_length - 1]) # -1 to preserve function with the 0vs1 base bit below
 
-    #Write a "bcftools consensus" friendly file of regions to mask
-    if mask_pos_list:
-        mask_file = open(args.output_name + '.tsv', 'w')
-        for mask_region in mask_pos_list:
-            #bcftools expects your mask file to be one based so need to add one to all co-ordinates
-            mask_file.write("%s\t%d\t%d\n" % (args.ref_name, mask_region[0] + 1, mask_region[-1] + 1 ))
-        mask_file.close()
+        #Write a "bcftools consensus" friendly file of regions to mask
+        if mask_pos_list:
+            mask_file = open(ref + '_mask.tsv', 'w')
+            for mask_region in mask_pos_list:
+                #bcftools expects your mask file to be one based so need to add one to all co-ordinates
+                mask_file.write("%s\t%d\t%d\n" % (ref, mask_region[0] + 1, mask_region[-1] + 1 ))
+            mask_file.close()
 
     #This bit actually masks a consensus file if you provide one 
     if args.fasta_to_mask:
-        with open(args.fasta_to_mask) as cns: #Open the input
-            for seq in SeqIO.parse(cns, "fasta"): #Get the seuqence bit
-                seq_list = []
-                if no_cov:
-                    masked_seq = "N" * seq_length
-                else:
-                    for base in seq: #Next section extracts the sequence to a list to allow masking with the coordinates produced earlier
-                        seq_list.append(base)
-                    for line in mask_pos_list:
-                        for i in line:
-                            seq_list[i] = "N"
-                    masked_seq = ''.join(seq_list)
-                new_record = SeqRecord(Seq(masked_seq), id=seq.id, name=seq.name, description=seq.description)
+        if not args.mmm:
+            with open(args.fasta_to_mask) as cns: #Open the input
+                for seq in SeqIO.parse(cns, "fasta"): #Get the seuqence bit
+                    seq_list = []
+                    if no_cov:
+                        masked_seq = "N" * seq_length
+                    else:
+                        for base in seq: #Next section extracts the sequence to a list to allow masking with the coordinates produced earlier
+                            seq_list.append(base)
+                        for line in mask_pos_list:
+                            for i in line:
+                                seq_list[i] = "N"
+                        masked_seq = ''.join(seq_list)
+                    new_record = SeqRecord(Seq(masked_seq), id=seq.id, name=seq.name, description=seq.description)
 
-                with open("%s.masked" % args.fasta_to_mask, "w") as output:
-                    SeqIO.write(new_record, output, "fasta")
-
+                    with open("%s.masked" % args.fasta_to_mask, "w") as output:
+                        SeqIO.write(new_record, output, "fasta")
+        else:
+            print (f"Can't mask fastas with Multi-Map Mode, rerun for the respective refs individually. The refs with greater than {args.reads} reads mapped are:\n{refs_above_threshold}")
 
 
     aln_file.close()
@@ -108,8 +126,13 @@ def main():
                             help='Mask a consensus sequence with your newly produced mask')
     optional_group.add_argument('-i', '--inverse', dest='inverse', action='store_true',
                             help='Return bed file of positions EQUAL OR ABOVE the chosen depth')
-    optional_group.add_argument('-v', '--version', action='version', version='maskara 1.1.3',
+    optional_group.add_argument('-v', '--version', action='version', version='maskara 1.1.4',
                                 help="Return Maskara version")
+    optional_group.add_argument('--mmm', dest='mmm', action='store_true',
+                                help="Multi-Map Mode: Get a depth mask for all references in your bam file with at least X reads")
+    optional_group.add_argument('--reads', dest='reads', default="50",
+                                help="Set the read limit for Multi-Map Mode. If a reference has at least this many reads it will have a depth mask made (note this is not the same as depth)")
+    
 
     parser.add_argument('input_file',
                             help='Path to the BAM file you want to create a mask for')
